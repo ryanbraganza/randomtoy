3d435b441756ceea2b3fc2d1f2dfe7eb6d75f729
Ryan Braganza <ryan.braganza@gmail.com>
2012-06-18 20:04:50 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cljs::Application.config.secret_token = '3a55475f75aff3b6bfc37437321f7d2ebe06541a7a99f15c27b39fd36ad2360d9b0ebb5cf0188b4123aef15baf05680e47e9768be84564758984c3f057e45272'
